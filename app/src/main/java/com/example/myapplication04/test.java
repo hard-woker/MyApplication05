4937d51cb1f7decee77d1b3873cf54d0a8b3783a
wangYinRui <1282929907@qq.com>
2022-08-04 11:17:34 +0800

    DESC:master 代码修改

--- app/src/main/java/com/example/myapplication04/test.java
+++ app/src/main/java/com/example/myapplication04/test.java
@@ -17,5 +17,6 @@ public class test {
         System.out.println("这是在分支myBranch中修改的内容");
         
         System.out.println("在github上面修改的内容");
+        System.out.println("ssh_在github上面修改的内容");
     }
 }
