de6036754138a333b2ca79381c9d2b1839c71ae3
wangYinRui <1282929907@qq.com>
2022-08-04 09:58:55 +0800

    添加的本地库

--- app/src/main/java/com/example/myapplication04/test.java
+++ app/src/main/java/com/example/myapplication04/test.java
@@ -3,5 +3,13 @@ package com.example.myapplication04;
 public class test {
     public static void main(String[] args) {
         System.out.println("这个是测试的方法");
+        System.out.println("这个是测试的方法");
+        System.out.println("这个是测试的方法");
+        System.out.println("这个是测试的方法");
+        System.out.println("这个是测试的方法");
+        System.out.println("这个是测试的方法");
+        System.out.println("这个是测试的方法");
+        System.out.println("这个是测试的方法");
+        System.out.println("这个是测试的方法");
     }
 }
